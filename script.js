3d1330e129d9b1198e21e7be8063f8e0b3e87057
Michaele Kowalski <159125720+micang7@users.noreply.github.com>
2025-10-28 08:59:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,110 @@
+(function () {
+  const example = document.getElementById("example");
+  const cw1 = document.getElementById("cw1");
+  const cw2 = document.getElementById("cw2");
+  const cw3 = document.getElementById("cw3");
+  const cw4 = document.getElementById("cw4");
+  const answer = document.getElementById("answer");
+
+  example.addEventListener("click", function () {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((array) => {
+        console.log(array);
+        answer.innerHTML = JSON.stringify(array);
+      });
+  });
+
+  function showLoadingPopup(title) {
+    const popup = document.createElement("div");
+    popup.classList.add("loading-popup");
+    popup.textContent = title;
+    document.body.appendChild(popup);
+  }
+
+  function hideLoadingPopup() {
+    const popup = document.querySelector(".loading-popup");
+    if (popup) document.body.removeChild(popup);
+  }
+
+  cw1.addEventListener("click", function () {
+    showLoadingPopup("Loading...");
+    setTimeout(
+      () =>
+        fetch("https://jsonplaceholder.typicode.com/posts")
+          .then((response) => response.json())
+          .then((array) => {
+            console.log(array);
+            console.log(array[0].title);
+            const list = array
+              .map(
+                (post) =>
+                  `<li class="post"><h3>${post.title}</h3><p>${post.body}</p></li>`,
+              )
+              .join("");
+            answer.innerHTML = `<ul>${list}</ul>`;
+            hideLoadingPopup();
+          }),
+      1000,
+    );
+  });
+
+  cw2.addEventListener("click", function () {
+    showLoadingPopup("Loading...");
+    setTimeout(
+      () =>
+        fetch("https://jsonplaceholder.typicode.com/posts/1")
+          .then((response) => response.json())
+          .then((post) => {
+            console.log(post);
+            answer.innerHTML = `<div class="post"><h3>${post.title}</h3><p>${post.body}</p></div>`;
+            hideLoadingPopup();
+          }),
+      1000,
+    );
+  });
+
+  cw3.addEventListener("click", function () {
+    showLoadingPopup("Processing...");
+    setTimeout(
+      () =>
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: "Metoda post",
+            body: "Metoda post pozwala przekazywać dane",
+            userId: 1,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            answer.innerHTML = `<p>Dodano nowy post o ID = ${data.id}</p><div class="post"><h3>${data.title}</h3><p>${data.body}</p></div>`;
+            hideLoadingPopup();
+          }),
+      1000,
+    );
+  });
+
+  cw4.addEventListener("click", function () {
+    showLoadingPopup("Loading...");
+    setTimeout(
+      () =>
+        fetch("https://my-json-server.typicode.com/micang7/json/posts")
+          .then((response) => response.json())
+          .then((array) => {
+            console.log(array);
+            console.log(array[0].title);
+            const list = array
+              .map((post) => `<li class="post"><h3>${post.title}</h3></li>`)
+              .join("");
+            answer.innerHTML = `<ul>${list}</ul>`;
+            hideLoadingPopup();
+          }),
+      1000,
+    );
+  });
+})();
